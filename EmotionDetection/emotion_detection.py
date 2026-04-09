de9188061b6cf0fee72059c53d229d75a165bfff
Ami L <ami.lanzinger@posteo.de>
2026-04-09 23:10:06 +0200

    Addd error handling to emotion_detection.py
    If response status code is not 200, return the dictionary with "None" values for all keys.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,13 +9,23 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     # Send POST request to API
     response = requests.post(url, json = myobj, headers=header)
-    # convert the response (string) to python dictionary
-    formatted_response = json.loads(response.text)
-    # extract the emotions as dictionary
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    # analyze the scores and determine the dominant emotion
-    dom_em = max(emotions, key=emotions.get) 
-    # add the dominant emotion
-    emotions['dominant_emotion'] = dom_em
-    # return a dict with emotions, score and most dominant emotion
+    
+    # Successful response
+    if response.status_code == 200:
+        # convert the response (string) to python dictionary
+        formatted_response = json.loads(response.text)
+        # extract the emotions as dictionary
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        # analyze the scores and determine the dominant emotion
+        dom_em = max(emotions, key=emotions.get) 
+        # add the dominant emotion to dict
+        emotions['dominant_emotion'] = dom_em
+    # Invalid response, server errors ...
+    else:
+        emotions = {'anger': None,
+                    'disgust': None,
+                    'fear': None,
+                    'joy': None,
+                    'sadness': None,
+                    'dominant_emotion': None}
     return emotions
