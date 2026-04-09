1ccf073dd248abad1a47e77c0c6eef7d462cc7ee
Ami L <ami.lanzinger@posteo.de>
2026-04-09 21:46:27 +0200

    Updating emotion_detection.py
    Upgrading function 'emotion_detector' to format the output as Python dictionary and determine the dominant emotion.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 #function with input variable "text_to_analyze"
 def emotion_detector(text_to_analyze):
@@ -8,5 +9,13 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     # Send POST request to API
     response = requests.post(url, json = myobj, headers=header)
-    # return response as text
-    return response.text
+    # convert the response (string) to python dictionary
+    formatted_response = json.loads(response.text)
+    # extract the emotions as dictionary
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    # analyze the scores and determine the dominant emotion
+    dom_em = max(emotions, key=emotions.get) 
+    # add the dominant emotion
+    emotions['dominant_emotion'] = dom_em
+    # return a dict with emotions, score and most dominant emotion
+    return emotions
